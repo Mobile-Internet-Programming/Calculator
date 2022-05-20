f70e60236a637fb97cec3fb08a75693058d586da
Gabriel Werner Strobl <68131643+DerCookieLiker@users.noreply.github.com>
2022-05-20 13:57:33 +0200

    [Feature] Simple

--- api/server.js
+++ api/server.js
@@ -2,32 +2,21 @@ let http = require('http');
 const url = require('url');
 
 const PORT = 8080;
+const calculate =  {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b 
+
+}
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
     let q = url.parse(req.url, true);
-    let calcObj = {
-        operand1: Number(q.query.operand1),
-        operator: decodeURIComponent(q.query.operator),
-        operand2: Number(q.query.operand2)
-    }
-    res.write(JSON.stringify(calculate(calcObj)));
+
+    res.write(JSON.stringify(calculate[decodeURIComponent(q.query.operator)](Number(q.query.operand1), Number(q.query.operand2))));
     res.end();
 }).listen(PORT, () => {
     console.log("Server started at port: " + PORT + " | http://localhost:" + PORT);
 });
 
-function calculate(calcObj) {
-    switch(calcObj.operator){
-        case '+': 
-            return calcObj.operand1 + calcObj.operand2;
-        case '-': 
-            return calcObj.operand1 - calcObj.operand2;
-        case '*': 
-            return calcObj.operand1 * calcObj.operand2;
-        case '/': 
-            return calcObj.operand1 / calcObj.operand2;
-        default: 
-            return 0;
-    }
-}
